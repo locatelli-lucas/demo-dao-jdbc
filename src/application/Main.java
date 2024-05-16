7bf289e6efa817ce5780c3ac8badb72f6defcb07
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-05-16 15:14:16 -0300

    findAll implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,5 +21,12 @@ public class Main {
         List<Seller> list = sellerDao.findByDepartment(department);
         for(Seller obj : list)
             System.out.println(obj);
+
+        System.out.println("\n==== TEST 3: seller findAll ====");
+        list = sellerDao.findAll();
+        for(Seller obj : list)
+            System.out.println(obj);
     }
+
+
 }
